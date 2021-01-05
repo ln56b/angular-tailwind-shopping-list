3b3f4eb374f9293ba34e7371ff78ffa21181511d
Hélène Baudrand <baudrand.helene@gmail.com>
2021-01-05 16:07:20 +0100

    routing initialisation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShoppingListDetailComponent } from './shopping-list/shopping-list-detail/shopping-list-detail.component';
+import { ShoppingListFormComponent } from './shopping-list/shopping-list-form/shopping-list-form.component';
+import { ShoppingListsComponent } from './shopping-list/shopping-lists/shopping-lists.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/lists', pathMatch: 'full' },
+  { path: 'lists', component: ShoppingListsComponent },
+  { path: 'lists/:id', component: ShoppingListDetailComponent },
+  { path: 'lists/:id/edit', component: ShoppingListFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
