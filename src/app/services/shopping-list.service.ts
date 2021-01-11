3c3c77624bfa84cfba70c4997b0b50a0f7686821
Hélène Baudrand <baudrand.helene@gmail.com>
2021-01-11 11:44:14 +0100

    shoppingList services

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -21,12 +21,43 @@ export class ShoppingListService {
       catchError(this.handleError('findAll', []))
     );
   }
+  getList(id: string): Observable<ShoppingList> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.get<ShoppingList>(url).pipe(
+      tap((list: ShoppingList) => console.log(`fetched list id: ${id}`)),
+      catchError(this.handleError<ShoppingList>('findOne'))
+    );
+  }
 
   addList(list: ShoppingList): Observable<ShoppingList> {
     return this.http.post<ShoppingList>(apiUrl, list, httpOptions).pipe(
       tap((list: ShoppingList) => console.log(`created list id: ${list._id}`)),
       catchError(this.handleError<ShoppingList>('addList'))
     );
+  }
+
+  updateList(list: ShoppingList): Observable<ShoppingList> {
+    const url = `${apiUrl}/${list._id}`;
+    return this.http.put<ShoppingList>(url, list, httpOptions).pipe(
+      tap((list: ShoppingList) => console.log(`updated list id: ${list._id}`)),
+      catchError(this.handleError<ShoppingList>('updateList'))
+    );
+  }
+
+  deleteList(id: string): Observable<ShoppingList> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.delete<ShoppingList>(url, httpOptions).pipe(
+      tap((_) => console.log(`deleted list id=${id}`)),
+      catchError(this.handleError<ShoppingList>('deleteList'))
+    );
+  }
+
+  save(list: ShoppingList): Observable<ShoppingList> {
+    if (list._id) {
+      return this.updateList(list).pipe(map(() => list));
+    } else {
+      return this.addList(list);
+    }
   }
 
   private handleError<T>(operation = 'operation', result?: T) {
